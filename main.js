ac969474257a7a18e66515d834383ecf29f205ed
Dirk Dobson <dirkvancouver@live.com>
2018-11-09 21:33:44 -0700

    collision detection

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ var bricks = []
 for(var c = 0; c < brickColumnCount; c++) {
   bricks[c] = [];
   for(var r = 0; r < brickRowCount; r++) {
-    bricks[c][r] = { x: 0, y: 0 };
+    bricks[c][r] = { x: 0, y: 0, status: 1 };
   }
 }
 
@@ -44,6 +44,7 @@ function draw() {
   x += dx;
   y += dy;
   drawPaddle();
+  collisionDetection();
   if(x + dx > canvas.width - ballRadius || x + dx < 0) {
     dx = -dx;
   }
@@ -77,15 +78,17 @@ function drawPaddle() {
 function drawBricks() {
   for(var c = 0; c < brickColumnCount; c++) {
     for(var r = 0; r < brickRowCount; r++) {
-      var brickX = ( c * (brickWidth + brickPadding)) + brickOffsetLeft;
-      var brickY = ( r * (brickHeight + brickPadding)) + brickOffsetTop;
-      bricks[c][r].x = brickX;
-      bricks[c][r].y = brickY;
-      ctx.beginPath();
-      ctx.rect(brickX, brickY, brickWidth, brickHeight);
-      ctx.fillStyle = "#0095DD";
-      ctx.fill();
-      ctx.closePath();
+      if(bricks[c][r].status == 1) {
+        var brickX = ( c * (brickWidth + brickPadding)) + brickOffsetLeft;
+        var brickY = ( r * (brickHeight + brickPadding)) + brickOffsetTop;
+        bricks[c][r].x = brickX;
+        bricks[c][r].y = brickY;
+        ctx.beginPath();
+        ctx.rect(brickX, brickY, brickWidth, brickHeight);
+        ctx.fillStyle = "#0095DD";
+        ctx.fill();
+        ctx.closePath();
+      }
     }
   }
 }
@@ -111,8 +114,11 @@ function collisionDetection() {
   for ( var c = 0; c < brickColumnCount; c++ ) {
     for ( var r = 0; r < brickRowCount; r++ ) {
       var b = bricks[c][r];
-      if ( x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
-        dy = dy;
+      if ( b.status == 1 ) {
+        if ( x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
+          dy = -dy;
+          b.status = 0;
+        }
       }
     }
   }
