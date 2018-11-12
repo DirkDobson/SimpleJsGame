9e8d77a1a88b1933a927f77e9c1ca1507ee92b85
Dirk Dobson <dirkvancouver@live.com>
2018-11-11 17:30:50 -0700

    mousemove eventListener

--- main.js
+++ main.js
@@ -30,6 +30,7 @@ for(var c = 0; c < brickColumnCount; c++) {
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
+document.addEventListener("mousemove", mouseMoveHandler, false);
 
 function drawBall() {
   ctx.beginPath();
